d84a5f828227b377ceb12e0ffabeed846fa03d23
NikitaaBhatt <nikitabhatt1218@gmail.com>
2026-06-04 17:48:15 +0530

    Setup frontend architecture and Tailwind CSS

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        TaskFlow App
+      </h1>
+    </div>
+  );
+}
+
+export default HomePage;
