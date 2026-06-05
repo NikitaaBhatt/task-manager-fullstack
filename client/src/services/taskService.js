93c582bb5d1dc31e526c14cb9d74c18750f8e6ff
Nikita Bhatt <156307179+NikitaaBhatt@users.noreply.github.com>
2026-06-05 16:42:28 +0530

    Connect frontend to deployed backend

--- client/src/services/taskService.js
+++ client/src/services/taskService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/tasks";
+const API_URL =
+  "https://taskflow-backend-c7p9.onrender.com/api/tasks";
 
 export const fetchTasks = async () => {
   const response = await axios.get(API_URL);
